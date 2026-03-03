e9600a13c90e8ca6bf8eb25bafdd73252cabb8ce
surenelee8 <surenelee8@gmail.com>
2026-03-03 09:41:39 -0800

    pls

--- script/expanding.js
+++ script/expanding.js
@@ -1,5 +1,16 @@
-panels = document.querySelectorALL('.panel')
+panels = document.querySelectorAll('.panel')
 
-for (i = 0; i < 5; i = i + 1){
-    panels[i].addEventListener
+panels[0].classList.add('active')
+
+for (let i = 0; i < 5; i = i + 1) {
+    panels[i].addEventListener('click', () => {
+        removeActiveFromAllPanels()
+        panels[i].classList.add('active')
+    })
+}
+
+function removeActiveFromAllPanels() {
+    for (let i = 0; i < 5; i = i + 1) {
+        panels[i].classList.remove('active')
+    }
 }
