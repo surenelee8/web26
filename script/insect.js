50ed8236dc7129aed737214078edfdc299b570c8
surenelee8 <surenelee8@gmail.com>
2026-03-20 16:37:44 +0000

    Fri, Mar 20, 2026, 9:37 AM -07:00

---
+++ script/insect.js
@@ -0,0 +1,30 @@
+screens = document.querySelectorAll('.screen')
+choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+start_btn = document.getElementById('start-btn')
+game_container = document.querySelector('.game-container')
+
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
+})
+
+for (let i = 0; i < choose_insect_btns.length; i = i + 1)
+{
+    choose_insect_btns[i].addEventListener('click', () => {
+        screens[1].classList.add('up')
+        img = choose_insect_btns[i].querySelector('img')
+        src = img.getAttribute('src')
+        alt = img.getAttribute('alt')
+        startGame()
+    })
+}
+
+function startGame() {
+    setTimeout(createInsect, 1000)
+}
+
+function createInsect() {
+    insect = document.createElement('div')
+    insect.classList.add('insect')
+    insect.innerHtml = `<img src="images/roachbg.png" alt="Roach">`
+    game_container.appendChild(insect)
+}
